0b3282c96c7a2d99bb2865e9f1970e7bc638d882
hyeongseo <luvvboy@gmail.com>
2020-07-24 15:55:30 +0900

    practice_ nodejs

---
+++ practice/practice.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const app = http.createServer(function(request,response){
+    let _url = request.url;
+    let queryData = url.parse(_url,true).query;
+    if(_url == '/'){
+        _url = '/index.html';
+    }
+    if(_url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(queryData.id);
+
+});
+app.listen(1010);
