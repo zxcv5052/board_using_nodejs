e6c9e5766383caee2f259c82752f9d0807119f32
hyeongseo <luvvboy@gmail.com>
2020-07-28 18:02:38 +0900

    JavaScript객체의 활용

--- practice/practice.js
+++ practice/practice.js
@@ -1,89 +1,162 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const qs = require('querystring');
 
-const app = http.createServer(function(request,response){
-    let _url = request.url;
-    let queryData = url.parse(_url,true).query;
-    let path_name = url.parse(_url,true).pathname;
-    let title = queryData.id;
-    if(path_name == '/'){
-        if(title == undefined){
-            fs.readdir('./data', ((err, files) => {
-                console.log(files);
-                title = 'welCome';
-                data = 'Hello, Node.js'
-                let filesList = '<ul>';
-                for(let i=0; i<files.length; i++){
-                    filesList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-                }
-                filesList += '</ul>';
-
-                let template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${filesList}
-              <h2>${title}</h2>
-              <p>
-                ${data}
-              </p>
-            </body>
-            </html>
-        
-            `;
-                response.writeHead(200);
-
-                response.end(template);
-            }))
-
-        }else{
-            fs.readdir('./data', ((err, files) => {
-                let filesList = '<ul>';
-                for (let i = 0; i < files.length; i++) {
-                    filesList += `<li><a href="/?id=${files[i]}">${files[i]}</a></li>`;
-                }
-                filesList += '</ul>';
-
-                fs.readFile(`data/${title}`, 'UTF-8', ((err, data) => {
-                    let template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${filesList}
-              <h2>${title}</h2>
-              <p>
-                ${data}
-              </p>
-            </body>
-            </html>
-        
-            `;
-                    response.writeHead(200);
-
-                    response.end(template);
-                }));
-            }));
+let template = {
+    HTML:function(title, list, body, control){
+        return `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      ${list}
+      ${control}
+      ${body}
+    </body>
+    </html>
+    `;
+    },list:function(filelist){
+        var list = '<ul>';
+        var i = 0;
+        while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
         }
-
-    }else{
-        response.writeHead(404);
-        response.end("NOT FOUND");
+        list = list+'</ul>';
+        return list;
     }
-    console.log();
+}
 
-
-
-
+let app = http.createServer(function(request,response){
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, filelist){
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = template.list(filelist);
+                var html = template.HTML(title, list,
+                    `<h2>${title}</h2>${description}`,
+                    `<a href="/create">create</a>`
+                );
+                response.writeHead(200);
+                response.end(html);
+            });
+        } else {
+            fs.readdir('./data', function(error, filelist){
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                    var title = queryData.id;
+                    var list = template.list(filelist);
+                    var html = template.HTML(title, list,
+                        `<h2>${title}</h2>${description}`,
+                        ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
+                    );
+                    response.writeHead(200);
+                    response.end(html);
+                });
+            });
+        }
+    } else if(pathname === '/create'){
+        fs.readdir('./data', function(error, filelist){
+            let title = 'WEB - create';
+            let list = template.list(filelist);
+            let html = template.HTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `, '');
+            response.writeHead(200);
+            response.end(html);
+        });
+    } else if(pathname === '/create_process'){
+        let body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            let post = qs.parse(body);
+            let title = post.title;
+            let description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();
+            })
+        });
+    } else if(pathname === '/update'){
+        fs.readdir('./data', function(error, filelist){
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                let title = queryData.id;
+                let list = template.list(filelist);
+                let html = template.HTML(title, list,
+                    `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
+                response.writeHead(200);
+                response.end(html);
+            });
+        });
+    } else if(pathname === '/update_process'){
+        let body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            let post = qs.parse(body);
+            let id = post.id;
+            let title = post.title;
+            let description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(error){
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                    response.writeHead(302, {Location: `/?id=${title}`});
+                    response.end();
+                })
+            });
+        });
+    } else if(pathname === '/delete_process'){
+        var body = '';
+        request.on('data', function(data){
+            body = body + data;
+        });
+        request.on('end', function(){
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error){
+                response.writeHead(302, {Location: `/`});
+                response.end();
+            })
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 });
 app.listen(1010);
