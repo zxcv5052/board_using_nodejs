afa55dbc121bb3eb4b37a188787175d1dbb90494
hyeongseo <luvvboy@gmail.com>
2020-08-26 00:29:43 +0900

    Client & Server 동시 구동 OK

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,23 @@
 import React,{ Component } from 'react';
 import Board from "./components/Board";
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles } from '@material-ui/core/styles'
+
+const style=theme => ({
+    root:{
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto'
+    },
+    table: {
+        minWidth: 1080
+    }
+});
 const boards =
 [
     {
@@ -21,26 +39,26 @@ const boards =
 ];
 class App extends Component {
     render() {
+        const {classes} = this.props;
         return(
-            <div>
-                {
-                    boards.map( c => {
-                        return(
-                            <Board
-                                key={c.id}
-                                id={c.id}
-                                title={c.title}
-                                contents={c.contents}
-                                register={c.register}
-                                regDate={c.regDate}
-                                readCnt={c.readCnt}
-                                />
-                        )
-                    })
-                }
-            </div>
+            <Paper className={classes.root}>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>번호</TableCell>
+                            <TableCell>작성자</TableCell>
+                            <TableCell>제목</TableCell>
+                            <TableCell>작성일</TableCell>
+                            <TableCell>조회수</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { boards.map( c => { return( <Board key={c.id} id={c.id} register={c.register} title={c.title} regDate={c.regDate} readCnt={c.readCnt}/>)})}
+                    </TableBody>
+                </Table>
+            </Paper>
         );
     }
 }
 
-export default App;
+export default withStyles(style)(App);
--- front/src/components/Board.js
+++ front/src/components/Board.js
@@ -1,38 +1,17 @@
 import React, { Component } from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-class Board extends React.Component{
+class Board extends Component{
     render() {
         return(
-          <div>
-              <BoardProfile id={this.props.id}/>
-              <BoardInfo title={this.props.title} contents={this.props.contents}
-                         register={this.props.register}
-                         regDate={this.props.regDate} readCnt={this.props.readCnt}/>
-          </div>
-        );
-    }
-}
-
-class BoardProfile extends React.Component{
-    render() {
-        return (
-            <div>
-                <h2>{this.props.id}</h2>
-            </div>
-        );
-    }
-}
-
-class BoardInfo extends React.Component{
-    render() {
-        return(
-          <div>
-              <h3>{this.props.title}</h3>
-              <h3>{this.props.contents}</h3>
-              <h3>{this.props.register}</h3>
-              <h3>{this.props.regDate}</h3>
-              <h3>{this.props.readCnt}</h3>
-          </div>
+            <TableRow>
+                <TableCell> {this.props.id} </TableCell>
+                <TableCell> {this.props.title} </TableCell>
+                <TableCell> {this.props.register}</TableCell>
+                <TableCell> {this.props.regDate} </TableCell>
+                <TableCell> {this.props.readCnt}</TableCell>
+            </TableRow>
         );
     }
 }
